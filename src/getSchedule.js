08dbcbfc34f4986a54ca34978a3b51187070f739
Mariana Ramiro <marianaramiroof@gmail.com>
2023-05-21 16:54:08 -0300

    'Parte do requisito 9'

--- src/getSchedule.js
+++ src/getSchedule.js
@@ -1,7 +1,22 @@
 const data = require('../data/zoo_data');
 
+const { species } = data;
+const { hours } = species;
+
 const getSchedule = (scheduleTarget) => {
-  // seu código aqui
+  if (scheduleTarget === species.name) {
+    return species.availability;
+  } if (!scheduleTarget) {
+    const schedule = {
+      Day : {
+        officeHour: 
+        exhibition: [],
+
+    }
+    return 
+  }
+
+
 };
 
 module.exports = getSchedule;
