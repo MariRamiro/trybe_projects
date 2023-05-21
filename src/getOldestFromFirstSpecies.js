38bedf4891219f6c742cf7dbfc403ae747882933
Mariana Ramiro <marianaramiroof@gmail.com>
2023-05-21 16:53:44 -0300

    'Requisito 10'

--- src/getOldestFromFirstSpecies.js
+++ src/getOldestFromFirstSpecies.js
@@ -1,7 +1,18 @@
 const data = require('../data/zoo_data');
 
 const getOldestFromFirstSpecies = (id) => {
-  // seu código aqui
+  const { employees } = data;
+  const firstZooId = employees.find((employee) => id === employee.id);
+  const [firstResident] = firstZooId.responsibleFor;
+  const { residents } = data.species.find(({ id: specieId }) => firstResident === specieId);
+  const oldestAge = residents.reduce((acc, cur) => {
+    if (acc.age > cur.age) {
+      return acc;
+    }
+    return cur;
+  });
+
+  return Object.values(oldestAge);
 };
 
 module.exports = getOldestFromFirstSpecies;
