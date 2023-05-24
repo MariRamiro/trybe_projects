9076135fdc4959a6f0fc42aad147e6a0ed9e0275
Mariana Ramiro <marianaramiroof@gmail.com>
2023-05-24 13:46:00 -0300

    Requisito 11

--- src/getEmployeesCoverage.js
+++ src/getEmployeesCoverage.js
@@ -1,16 +1,49 @@
 const data = require('../data/zoo_data');
 
-// const { species } = data;
-// const { employees } = data;
+const { species } = data;
+const { employees } = data;
 
-const getEmployeesCoverage = () => {};
+const getEmployee = (colaborators) => employees.find((employee) =>
+  employee.firstName === colaborators.name
+  || employee.lastName === colaborators.name
+  || employee.id === colaborators.id);
 
-// //   const getAnimal = (arr) => species.find((specie) => specie.id === arr.id);
-// //   const getEmployee = (obj) => employees
-// //     .find((employee) => obj.name === employee.firstName || obj
-// //       .name === employee.lastName || obj.id === employee.id);
+const getAnimals = (animalsArray) => species.filter((animal) =>
+  animalsArray.some((idanim) => animal.id === idanim));
 
-// //       return getAnimal; getEmployee
-// // };
+const generateArrData = (arrayOfAnimals) => {
+  const arrayOfSpecies = [];
+  const arrayOfLocation = [];
+  arrayOfAnimals.forEach((animal) => {
+    arrayOfSpecies.push(animal.name);
+    arrayOfLocation.push(animal.location);
+  });
+  return [arrayOfSpecies, arrayOfLocation];
+};
+
+const generateObject = (colaborators, animalsFunction, dataFunction) => {
+  const { id, firstName, lastName, responsibleFor } = colaborators;
+  const arr = animalsFunction(responsibleFor);
+  const [arrayOfSpecies, arrayOfLocation] = dataFunction(arr);
+
+  return {
+    id,
+    fullName: firstName.concat(' ', lastName),
+    species: arrayOfSpecies,
+    locations: arrayOfLocation,
+  };
+};
+const getEmployeesCoverage = (param) => {
+  if (!param) {
+    return employees.map((employee) => generateObject(employee, getAnimals, generateArrData));
+  }
+  if (param.name || param.id) {
+    const employee = getEmployee(param);
+    if (!employee) {
+      throw new Error('Informações inválidas');
+    }
+    return generateObject(employee, getAnimals, generateArrData);
+  }
+};
 
 module.exports = getEmployeesCoverage;
